321d8afe5af31127aff9ed0096b962194446664d
Matthmul <73168857+Matthmul@users.noreply.github.com>
2022-06-15 12:20:45 +0200

    Axios fix

--- frontend/src/config/backendConfig.js
+++ frontend/src/config/backendConfig.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: `https://the-shop-backend.azurewebsites.net/`,
 
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:9000, https://the-shop-backend.azurewebsites.net/',
+    'Access-Control-Allow-Origin': 'http://localhost:9000, https://the-shop-backend.azurewebsites.net:9000',
     'Content-Type': 'application/json',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
