414bbcd019738cec3160d00395bf37ac41db4bbe
Matthmul <73168857+Matthmul@users.noreply.github.com>
2022-06-15 17:04:26 +0200

    allow host

--- frontend/src/config/backendConfig.js
+++ frontend/src/config/backendConfig.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: `https://the-shop-backend.azurewebsites.net/`,
 
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:9000, https://the-shop-backend.azurewebsites.net:9000',
+    'Access-Control-Allow-Origin': 'http://localhost:8081, https://the-shop-backend.azurewebsites.net:8081',
     'Content-Type': 'application/json',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
