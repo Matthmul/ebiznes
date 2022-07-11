48ed87a52d9b2bddf14bdee205532958b82fe8d2
Matthmul <73168857+Matthmul@users.noreply.github.com>
2022-07-11 22:31:40 +0200

    Fix

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -4,8 +4,7 @@ import { loadStripe } from "@stripe/stripe-js"
 import PaymentForm from "../form/PaymentForm"
 
 const Payment = () => {
-    const STRIPE_PUBLIC = 'pk_test_51LEgHTL0G5tfLaRaqdB7xw8sPxlyunLymkdjfN6S6AvGGnHo8xarUPw7KvF7cpUc9CqXDvlEaOfnoVcckUE9PHIc00djvOImGC'
-    const stripeTestPromise = loadStripe(STRIPE_PUBLIC)
+    const stripeTestPromise = loadStripe(`${process.env.STRIPE_PUBLIC}`);
 
     return (
         <Elements stripe={stripeTestPromise}>
